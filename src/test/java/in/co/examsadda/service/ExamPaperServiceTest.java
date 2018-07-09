3b881feb5502071dd26b83e51cca50373caca47b
ssd-exams-adda <sap.koduri@hotmail.com>
2018-07-09 11:05:31 +0530

    Exam Paper Service test added

---
+++ src/test/java/in/co/examsadda/service/ExamPaperServiceTest.java
@@ -0,0 +1,33 @@
+package in.co.examsadda.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import in.co.examsadda.model.ExamPaper;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamPaperServiceTest {
+	
+	@Autowired
+	private ExamPaperService examPaperService;
+	
+	@Test
+	public void getExamPaperByExamPaperIdTest() throws Exception {
+		
+		try {
+			ExamPaper examPaperByExamPaperId = examPaperService.getExamPaperByExamPaperId(3);
+			System.err.println(examPaperByExamPaperId);
+			Assert.assertNotEquals(null, examPaperByExamPaperId.getPracticePaper());
+			Assert.assertNotEquals(true, examPaperByExamPaperId.getSections().isEmpty());
+		} catch (javax.persistence.EntityNotFoundException e) {
+			System.err.println("Record not found");
+		}
+	}
+}
